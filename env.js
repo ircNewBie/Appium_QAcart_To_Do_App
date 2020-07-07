eea096cc869a208f17558a44def65f78619aeed1
hatem <hatem.hatamleh@gmail.com>
2020-07-07 11:52:27 +0200

    react native project

--- env.js
+++ env.js
@@ -1,11 +1,5 @@
 const config = {
-  apiKey: "AIzaSyB0UwfTUNG2iGry57aJnTfZgi265ie0bz0",
-  authDomain: "to-do-97442.firebaseapp.com",
-  databaseURL: "https://to-do-97442.firebaseio.com",
-  projectId: "to-do-97442",
-  storageBucket: "to-do-97442.appspot.com",
-  messagingSenderId: "621755693802",
-  appId: "1:621755693802:web:21f84ae86de45800267593"
+
 };
 
 export default config;
