ddc6c64ededccc19d77eda1d3a91ddedb25faa48
= <=>
2021-06-08 19:19:56 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,12 @@
 const config = {
-
+    // Your web app's Firebase configuration
+    apiKey: "AIzaSyBDp-fXijH1XppUhdTA5FlipUVEDgwo18Q",
+    authDomain: "react-native-todo-1ed92.firebaseapp.com",
+    projectId: "react-native-todo-1ed92",
+    storageBucket: "react-native-todo-1ed92.appspot.com",
+    messagingSenderId: "234158112340",
+    appId: "1:234158112340:web:3360d80fc2159e0c95b7c1"
+  
 };
 
 export default config;
